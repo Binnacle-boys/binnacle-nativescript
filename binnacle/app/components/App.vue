b5888f8de60777cacb78fc6369006b3702a19775
Donovan Rost <drost@ucsc.edu>
2019-01-23 14:01:18 -0800

    initial commit

---
+++ binnacle/app/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NatnasdiveScript-Vue!"/>
+        <GridLayout columns="*" rows="*">
+            <Label class="message" :text="msg" col="0" row="0"/>
+        </GridLayout>
+    </Page>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        msg: 'Hello Worlsasd99999d!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ binnacle/app/main.ts
@@ -0,0 +1,13 @@
+import Vue from 'nativescript-vue'
+import App from './components/App.vue'
+
+if(TNS_ENV !== 'production') {
+//   Vue.use(VueDevtools)
+}
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production')
+
+
+new Vue({
+  render: h => h('frame', [h(App)])
+}).$start()
