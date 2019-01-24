695a3cd8a0084237b2ffe8a46e1ef9696d76cd17
Donovan Rost <drost@ucsc.edu>
2019-01-23 16:23:48 -0800

    geolocation plugin added

--- binnacle/app/main.ts
+++ binnacle/app/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'nativescript-vue'
 import App from './components/App.vue'
+import * as geolocation from "nativescript-geolocation";
 
 if(TNS_ENV !== 'production') {
 //   Vue.use(VueDevtools)
@@ -7,6 +8,8 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
+
+geolocation.enableLocationRequest();
 
 new Vue({
   render: h => h('frame', [h(App)])
