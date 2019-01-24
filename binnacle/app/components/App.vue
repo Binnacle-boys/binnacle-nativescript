6d119870a0339f0d574c6902f21d802af4d298dd
dan-rds <ddrichar@ucsc.edu>
2019-01-23 18:54:07 -0800

    working accelerometer, just in app.vue for now, will reorganize after we agree on structure of comps

--- binnacle/app/components/App.vue
+++ binnacle/app/components/App.vue
@@ -1,95 +1,54 @@
 <template>
     <Page class="page">
-        <ActionBar class="action-bar">
-            <Label class="action-bar-title" text="Geolocation Vue Demo"></Label>
+        <ActionBar class="action-bar" style="background: #777">
+            <Label class="action-bar-title" text="Accelerometer Vue Demo"></Label>
         </ActionBar>
 
-        <GridLayout rows="auto, auto, *, auto">
-            <GridLayout row="0" columns="*, *, *, *" >
-                <Button text="Enable Location" col="0" textWrap="true" @tap="enableLocationTap"/>
-                <Button text="Get Current Location" col="1" textWrap="true" @tap="buttonGetLocationTap"/>
-                <Button text="Start Monitoring" col="2" textWrap="true" @tap="buttonStartTap"/>
-                <Button text="Stop Monitoring" col="3" textWrap="true" @tap="buttonStopTap"/>
-            </GridLayout>
-            <ListView row="2" for="item in locations">
-                <v-template>
-                    <Label :text="item.latitude + ', ' + item.longitude + ', ' + item.altitude" />
-                </v-template>
-            </ListView>
-            <Button text="Clear" row="3" @tap="buttonClearTap"/>
-        </GridLayout>
+        <ScrollView>
+            <StackLayout class="home-panel">
+                <StackLayout>
+                    <Button text="Start Accelerometer" @tap="startAccelerometer"
+                        :visibility="accelerometerValues.x ? 'collapsed' : 'visible'" />
+                    <StackLayout :visibility="accelerometerValues.x ? 'visible' : 'collapsed'">
+                        <Label :text="'X: ' + accelerometerValues.x" />
+                        <Label :text="'Y: ' + accelerometerValues.y" />
+                        <Label :text="'Z: ' + accelerometerValues.z" />
+                    </StackLayout>
+                </StackLayout>
+        </StackLayout>
+    </ScrollView>
     </Page>
 </template>
 
 <script>
-    import * as geolocation from "nativescript-geolocation";
-    import { Accuracy } from "tns-core-modules/ui/enums";
+    const accelerometer = require("nativescript-accelerometer");
+    let accelerometerListening = false;
 
     export default {
-        data() {
-            return {
-                watchIds: [],
-                locations: []
+        methods: {
+            startAccelerometer: function() {
+                if (accelerometerListening) {
+                    accelerometer.stopAccelerometerUpdates();
+                }
+                accelerometer.startAccelerometerUpdates(
+                    data => {
+                        accelerometerListening = true;
+                        this.accelerometerValues = data;
+                    }, {
+                        sensorDelay: "ui"
+                    }
+                );
             }
         },
-        methods: {
-            enableLocationTap: function() {
-                geolocation.isEnabled().then(function (isEnabled) {
-                    if (!isEnabled) {
-                        geolocation.enableLocationRequest().then(function () { }, function (e) {
-                            console.log("Error: " + (e.message || e));
-                        });
-                    }
-                }, function (e) {
-                    console.log("Error: " + (e.message || e));
-                });
-            },
-            buttonGetLocationTap: function() {
-                let that = this;
-                geolocation.getCurrentLocation({
-                    desiredAccuracy: Accuracy.high,
-                    maximumAge: 5000,
-                    timeout: 10000
-                }).then(function (loc) {
-                    if (loc) {
-                        that.locations.push(loc);
-                    }
-                }, function (e) {
-                    console.log("Error: " + (e.message || e));
-                });
-            },
-            buttonStartTap: function() {
-                try {
-                    let that = this;
-                    this.watchIds.push(geolocation.watchLocation(
-                        function (loc) {
-                            if (loc) {
-                                that.locations.push(loc);
-                            }
-                        },
-                        function (e) {
-                            console.log("Error: " + e.message);
-                        },
-                        {
-                            desiredAccuracy: Accuracy.high,
-                            updateDistance: 1,
-                            updateTime: 3000,
-                            minimumUpdateTime: 100
-                        }));
-                } catch (ex) {
-                    console.log("Error: " + ex.message);
+
+        data() {
+            return {
+                accelerometerValues: {
+                    x: null,
+                    y: null,
+                    z: null
                 }
-            },
-            buttonStopTap: function() {
-                let watchId = this.watchIds.pop();
-                while (watchId != null) {
-                    geolocation.clearWatch(watchId);
-                    watchId = this.watchIds.pop();
-                }
-            },
-            buttonClearTap: function() {
-                this.locations.splice(0, this.locations.length);
-            }
+            };
         }
     };
 </script>
