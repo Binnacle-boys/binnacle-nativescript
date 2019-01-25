7abea734d46cc09e1c1b84bcc7021f6522e606cf
Casey Hillers <caseyhillers@gmail.com>
2019-01-25 15:27:29 -0800

    tslint fix

--- binnacle/app/main.ts
+++ binnacle/app/main.ts
@@ -1,9 +1,10 @@
+import * as geolocation from 'nativescript-geolocation';
 import Vue from 'nativescript-vue';
-import App from './components/App.vue';
-import * as geolocation from "nativescript-geolocation";
 
-if(TNS_ENV !== 'production') {
-//   Vue.use(VueDevtools)
+import App from './components/App.vue';
+
+if (TNS_ENV !== 'production') {
+  //   Vue.use(VueDevtools)
 }
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
@@ -11,6 +12,4 @@ Vue.config.silent = (TNS_ENV === 'production');
 
 geolocation.enableLocationRequest();
 
-new Vue({
-  render: h => h('frame', [h(App)])
-}).$start();
+new Vue({render: h => h('frame', [h(App)])}).$start();
