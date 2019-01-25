8463ff513e79116b1dfa405b84a22859e2690156
Casey Hillers <caseyhillers@gmail.com>
2019-01-25 14:59:26 -0800

    tslint with google ts-style

--- binnacle/app/main.ts
+++ binnacle/app/main.ts
@@ -1,16 +1,16 @@
-import Vue from 'nativescript-vue'
-import App from './components/App.vue'
+import Vue from 'nativescript-vue';
+import App from './components/App.vue';
 import * as geolocation from "nativescript-geolocation";
 
 if(TNS_ENV !== 'production') {
 //   Vue.use(VueDevtools)
 }
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production')
+Vue.config.silent = (TNS_ENV === 'production');
 
 
 geolocation.enableLocationRequest();
 
 new Vue({
   render: h => h('frame', [h(App)])
-}).$start()
+}).$start();
