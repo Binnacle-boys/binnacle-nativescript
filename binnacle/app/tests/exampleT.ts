568b163e2c8c347e53a8256f344e3867c9e778c6
nkalscheuer <nkalscheuer@gmail.com>
2019-01-26 15:22:27 -0800

    Added unit testing

---
+++ binnacle/app/tests/exampleT.ts
@@ -0,0 +1,9 @@
+// we need to include the reflect-metadata dependency.
+import "reflect-metadata"; 
+
+// A sample Jasmine test
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
